be30ae06484121a1ff5d534e1be98aea429dd7a4
nisha5hetty <86158263+nisha5hetty@users.noreply.github.com>
2021-07-23 13:52:58 +0530

    configured it to send email

--- dictionaryscraper.py
+++ dictionaryscraper.py
@@ -8,6 +8,10 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
+import base64
 
 # In[15]:
 
@@ -62,6 +66,34 @@ df
 
 df.to_csv("wordoftheday.csv, index=False")
 
+message = Mail(
+    from_email=os.environ.get('FROM_EMAIL'),
+    to_emails=os.environ.get('TO_EMAIL'),
+    subject='Scraped content',
+    html_content="It's attached as an... attachment.")
+
+# https://www.twilio.com/blog/sending-email-attachments-with-twilio-sendgrid-python
+with open('wordoftheday.csv', 'rb') as f:
+    data = f.read()
+    f.close()
+encoded_file = base64.b64encode(data).decode()
+
+attachedFile = Attachment(
+    FileContent(encoded_file),
+    FileName('wordoftheday.csv'),
+    FileType('text/csv'),
+    Disposition('attachment')
+)
+message.attachment = attachedFile
+
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
 
 # In[ ]:
 
